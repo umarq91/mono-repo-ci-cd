efb39a3defb56ca898813005c429f1d6851fb28b
Muhammad Umar Qureshi <m.umarqureshi091@gmail.com>
2025-07-17 20:58:58 +0500

    initial prisma setup

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,9 @@
+// eslint-disable-next-line    
+
 import Image, { type ImageProps } from "next/image";
 import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
+import { client } from "@repo/db/client";
 
 type Props = Omit<ImageProps, "src"> & {
   srcLight: string;
